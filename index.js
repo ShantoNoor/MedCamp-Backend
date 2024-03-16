481a75e82aef890f877e500649ba3f428f15d6e4
Shanto Noor <shantonoor900@gmail.com>
2024-03-16 19:07:08 +0600

    sign-in sign-up added

--- index.js
+++ index.js
@@ -23,6 +23,32 @@ app.get("/", async (req, res) => {
   return res.send("MedCamp server is Running");
 });
 
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const result = await user.save();
+    return res.status(201).send(result);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return res.status(400).send(err.message);
+    } else {
+      return res.status(409).send("User already exists");
+    }
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    return res.send(await User.find(req.query));
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return res.status(400).send(err.message);
+    } else {
+      return res.status(500).send("Something went wrong");
+    }
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
